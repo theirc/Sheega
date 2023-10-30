e58731477809642b5c3b4150f9d50046451f875c
Emilio Guisolfo <emilioguisolfo@gmail.com>
2023-10-30 11:46:03 -0300

    feat: update theme files (#7)
    Co-authored-by: Chris Anyango <chris@maisoninteractive.agency>

--- cypress/e2e/locale-select.cy.ts
+++ cypress/e2e/locale-select.cy.ts
@@ -1,21 +1,21 @@
 describe('locale select', { defaultCommandTimeout: 20000 }, () => {
   it('redirects based on local storage', () => {
-    localStorage.setItem('preferredLocaleJSON', '"en-us"');
+    localStorage.setItem('preferredLocaleJSON', '"fr"');
     cy.visit(encodeURI('http://localhost:3003/locale-select?target=/'));
-    cy.url().should('eq', 'http://localhost:3003/en-us');
+    cy.url().should('eq', 'http://localhost:3003/fr');
   });
 
   it('if a user clicks on locale, moves to that page', () => {
     cy.visit(encodeURI('http://localhost:3003/locale-select?target=/'));
-    cy.get("[href='/en-us']").click();
-    cy.url().should('eq', 'http://localhost:3003/en-us');
+    cy.get("[href='/fr']").click();
+    cy.url().should('eq', 'http://localhost:3003/fr');
   });
 
   it('if a user clicks on locale and preferred locale is gibberrish, still works', () => {
     localStorage.setItem('preferredLocaleJSON', '"rubbish"');
     cy.visit(encodeURI('http://localhost:3003/locale-select?target=/'));
-    cy.get("[href='/en-us']").click();
-    cy.url().should('eq', 'http://localhost:3003/en-us');
+    cy.get("[href='/fr']").click();
+    cy.url().should('eq', 'http://localhost:3003/fr');
   });
 });
 
